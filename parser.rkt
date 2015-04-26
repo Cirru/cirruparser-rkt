1be308e30608c527c67d2a5f98d0bb161782a60e
jiyinyiyong <jiyinyiyong@gmail.com>
2015-04-26 16:31:58 +0800

    blind convert

--- parser.rkt
+++ parser.rkt
@@ -1,8 +1,297 @@
 #lang racket
 
-(provide demo)
+(require "tree.rkt")
 
-(define (demo)
-  (display "demo"))
+(provide parse pare)
 
-(display "parser loaded")
+(define (parse code filename)
+  (define buffer (make-hash))
+
+  (define state (make-hash))
+  (hash-set! state 'name 'indent)
+  (hash-set! state 'x 1)
+  (hash-set! state 'y 1)
+  (hash-set! state 'level 1)
+  (hash-set! state 'indent 0)
+  (hash-set! state 'indented 0)
+  (hash-set! state 'nest 0)
+  (hash-set! state 'path filename)
+
+  (define res (parsing (list) buffer state code))
+  (define res (map resolve-dollar res))
+  (define res (map resolve-comma res))
+  res)
+
+(define (pare code filename)
+  (shorten (parse code filename)))
+
+(define (shorten x)
+  (if (list? x)
+    (map shorten x)
+    (hash-ref x 'text)))
+
+(define (escape-eof xs buffer state code)
+  (error "EOF in escape state"))
+
+(define (string-eof xs buffer state code)
+  (error "EDOT in string state"))
+
+(define (space-eof xs buffer state code)
+  xs)
+
+(define (token-eof xs buffer state code)
+  xs)
+
+;; escape
+
+(define (escape-newline xs buffer state code)
+  (error "new line while escape"))
+
+(define (escape-n xs buffer state code)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x)))
+  (hash-set! buffer 'text
+    (string-append (hash-ref buffer 'text) "\n"))
+  (hash-set! state 'name 'string)
+  (parsing xs buffer state (substring code 1)))
+
+(define (escape-t xs buffer state code)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (hash-set! buffer 'text
+    (string-append (hash-ref buffer 'text) "\n"))
+  (hash-set! state 'name 'string)
+  (parsing xs buffer state (substring code 1)))
+
+(define (escape-else xs buffer state code)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (hash-set! buffer 'text
+    (string-append (hash-ref buffer 'text)
+      (substring code 0 1)))
+  (hash-set! state 'name 'string)
+  (parsing xs buffer state (substring code 1)))
+
+;; string
+
+(define (string-backslash xs buffer state code)
+  (hash-set! state :name 'escape)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (string-newline xs buffer state code)
+  (error "newline in a string"))
+
+(define (string-quote xs buffer state code)
+  (hash-set! state 'name 'token)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (string-else xs buffer state code)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (hash-set! buffer 'text (substring code 0 1))
+  (parsing xs buffer state (substring code 1)))
+
+;; space
+
+(define (space-space xs buffer state code)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (space-newline xs buffer state code)
+  (if
+    (not (equal? (hash-ref state 'nest) 0))
+    (error "incorrect nesting"))
+  (hash-set! state 'name 'indent)
+  (hash-set! state 'x 1)
+  (hash-set! state 'y
+    (+ (hash-ref state 'y) 1))
+  (hash-set! state 'indented 0)
+  (parsing xs buffer state (substring code 1)))
+
+(define (space-open xs buffer state code)
+  (define nesting (create-nesting 1))
+  (define xs (append-item xs (hash-ref state 'level) nesting))
+  (hash-set! state 'nest
+    (+ (hash-ref state 'nest) 1))
+  (hash-set! state 'level
+    (+ (hash-ref state 'level) 1))
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (space-close xs buffer state code)
+  (hash-set! state 'nest
+    (- (hash-ref state 'nest) 1))
+  (hash-set! state 'level
+    (- (hash-ref state 'level) 1))
+  (if (< (hash-ref state 'nest) 0)
+    (error "close at space"))
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (space-quote xs buffer state code)
+  (hash-set! state 'name 'string)
+
+  (define buffer (make-hash))
+  (hash-set! buffer 'text "")
+  (hash-set! buffer 'x (hash-ref state 'x))
+  (hash-set! buffer 'y (hash-ref state 'y))
+  (hash-set! state 'x
+    (+ (hash-ref state 'path) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (space-else xs buffer state code)
+  (hash-set! state'name 'token)
+
+  (define buffer (make-hash))
+  (hash-set! buffer 'text (substring code 0 1))
+  (hash-set! buffer 'x (hash-ref state 'x))
+  (hash-set! buffer 'y (hash-ref state 'y))
+  (hash-set! buffer 'path (hash-ref state 'path))
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+;; token
+
+(define (token-space xs buffer state code)
+  (hash-set! state 'name 'space)
+  (hash-set! buffer 'ex (hash-ref state 'x))
+  (hash-set! buffer 'ey (hash-ref state 'y))
+  (define xs (append-item xs (hash-ref state 'level) buffer))
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (define buffer (make-hash))
+  (parsing xs buffer state (substring code 1)))
+
+(define (token-newline xs buffer state code)
+  (hash-set! state 'name 'indent)
+  (hash-set! buffer 'ex (hash-ref state 'x))
+  (hash-set! buffer 'ey (hash-ref state 'y))
+  (define xs (append-item xs (hash-ref state 'level) buffer))
+  (hash-set! state 'indented 0)
+  (hash-set! state 'x 1)
+  (hash-set! state 'y
+    (+ hash-ref state 'y) 1)
+  (define buffer (make-hash))
+  (parsing xs buffer state (substring code 1)))
+
+(define (token-open xs buffer state code)
+  (error "open parenthesis in token"))
+
+(define (token-close xs buffer state code)
+  (hash-set! state 'name 'space)
+  (hash-set! buffer 'ex (hash-ref state 'x))
+  (hash-ref! buffer 'ey (hash-ref state 'y))
+  (define xs (append-item xs (hash-ref state 'level buffer)))
+  (define buffer (make-hash))
+  (parsing xs buffer state code))
+
+(define (token-quote xs buffer state code)
+  (hash-set! state 'name 'string)
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (token-else xs buffer state code)
+  (hash-set! buffer 'text
+    (string-append (hash-ref buffer 'text) (substring code 0 1)))
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+;; indent
+
+(define (indent-space xs buffer state code)
+  (hash-set! state 'indented
+    (+ (hash-ref state 'indented) 1))
+  (hash-set! state 'x
+    (+ (hash-ref state 'x) 1))
+  (parsing xs buffer state (substring code 1)))
+
+(define (indent-newline xs buffer state code)
+  (hash-set! state 'x 1)
+  (hash-set! state 'y
+    (+ (hash-ref state 'y) 1))
+  (hash-set! state 'indented 0)
+  (parsing xs buffer state (substring code 1)))
+
+(define (indent-close xs buffer state code)
+  (error "close parenthesis at indent"))
+
+(define (indent-else xs buffer state code)
+  (hash-set! state 'name 'space)
+  (if
+    (equal? (remainder (hash-ref state 'indented) 2) 1)
+    (error "odd indnetation"))
+  (define indented (remainder (hash-ref state 'indented 2)))
+  (define diff (- indented (hash-ref state 'indent)))
+
+  (define level
+    (if (<= diff 0)
+      (- (+ (hash-ref state 'level) diff) 1)
+      (hash-ref state 'level)))
+  (define nesting
+    (if (<= diff 0)
+      (create-nesting 1)
+      (create-nesting diff)))
+  (define xs (append-item xs level nesting))
+  (hash-set! state 'level
+    (+ (hash-ref state 'level) 1))
+  (hash-set! state 'indented indented)
+  (parsing xs buffer state code))
+
+(define (parse xs buffer state code)
+  (define eof (equal? (string-length code) 0))
+  (define char (if eof "" (substring code 0 1)))
+  (define state-name (hash-ref state 'name))
+  (define method (cond
+    ((equal? state-name 'escape)
+      (if eof escape-eof
+        (cond
+          ((equal? char "\n") escape-newline)
+          ((equal? char "n")(escape-n)
+          ((equal? char "t")(escape-t)
+          (else escape-else) )))
+
+    ((equal? state-name 'string)
+      (if eof string-eof
+        (cond
+          ((equal? char "\\") string-backslash)
+          ((equal? char "\n") string-newline)
+          ((equal? char "\"") string-quote)
+          (else string-else) )))
+
+    ((equal? state-name 'space)
+      (if eof space-eof
+        (cond
+          ((equal? char " ") space-space)
+          ((equal? char "\n") space-newline)
+          ((equal? char "(") space-open)
+          ((equal? char ")") space-close)
+          ((equal? char "\"") space-quote)
+          (else space-else) )))
+    ((equal? state-name 'token)
+      (if eof token-eof
+        (cond
+          ((equal? char " ") token-space)
+          ((equal? char "\n") token-newline)
+          ((equal? char "(") token-open)
+          ((equal? char ")") token-close)
+          ((equal? char "\"") token-quote)
+          (else token-else))))
+    ((equal? state-name 'indent)
+      (if eof indent-close
+        (cond
+          ((equal? char " ") indent-space)
+          ((equal? char "\n") indent-space)
+          ((equal? char "\)") indent-close)
+          (else indent-else))))
+  ))
+  (method xs buffer state code))
---
+++ tree.rkt
@@ -0,0 +1,64 @@
+
+#lang racket
+
+(provide append-item
+  create-nesting
+  resolve-dollar
+  resolve-comma)
+
+(define (cirru-append xs level item)
+  (if (equal? level 0)
+    (append xs (list item))
+    (append (drop-right xs 1) (list
+      (append-item (last xs) (- level 1) item)))))
+
+(define (nesting-helper xs n)
+  (if (<= n 1) xs
+    (list (nesting-helper xs (- n 1)))))
+
+(define (create-nesting n)
+  (nesting-helper (list) n))
+
+(define (dollar-helper before after)
+  (if (equal? (length after) 0) before
+    (let
+      ((cursor (first after)))
+      (cond
+        ((list? cursor)
+          (dollar-helper
+            (append before (resolve-dollar cursor))
+            (rest after)))
+        ((equal? (hash-ref cursor 'text) "$")
+          (append before (resolve-dollar (rest after))))
+        (else (dollar-helper
+          (append before (list cursor)) (rest after)))))))
+
+(define (resolve-dollar xs)
+  (if (equal? (length xs) 0) xs
+    (dollar-helper (list) xs)))
+
+(define (comma-helper before after)
+  (if (equal? (length after) 0) before
+    (let
+      ((cursor (first after)))
+      (if (and (list? cursor) (> (length cursor) 0))
+        (let
+          ((head (first cursor)))
+          (cond
+            ((list? head)
+              (comma-helper
+                (append before (list (resolve-comma cursor)))
+                (rest after)))
+            ((equal? (hash-ref head 'text) ",")
+              (comma-helper before
+                (append (resolve-comma (rest cursor))
+                  (rest after))))
+            (else (comma-helper
+              (append before (list (resolve-comma cursor)))
+              (rest after)))))
+        (comma-helper (append before (list after))
+          (rest after))))))
+
+(define (resolve-comma xs)
+  (if (equal? (length xs) 0 ) xs
+    (comma-helper (list) xs)))
